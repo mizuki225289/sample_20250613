4775c67375c9e1b30a908197c540f0f916cd62fd
mikoshiba <m2210610@gl.cc.uec.ac.jp>
2025-06-13 11:27:37 +0900

    add func

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int fucn(int x) {
+  return x*x;
+}
+
 int main(void) {
   std::cout << "git" << std::endl;
   std::cout << "Hello!" << std::endl;
@@ -8,5 +12,7 @@ int main(void) {
     std::cout << i << std::endl;
   }
 
+  std::cout << func(6) << std::endl;
+  
   return 0;
 }
