9957d775e22d1d4be07dce3206e8524549e1b20c
mikoshiba <m2210610@gl.cc.uec.ac.jp>
2025-06-13 12:08:42 +0900

    Add func2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ int fucn(int x) {
   return x*x;
 }
 
+int func2(int x, int y) {
+  return x+y;
+}
+
 int main(void) {
   std::cout << "git" << std::endl;
   std::cout << "Hello!" << std::endl;
@@ -13,6 +17,7 @@ int main(void) {
   }
 
   std::cout << func(6) << std::endl;
+  std::cout << func2(5,7) << syd::endl;
   
   return 0;
 }
